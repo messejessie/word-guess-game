3f3a3d7ed0a1399df0e639c1fc8d4d4235542764
messejessie <messejessie77@gmail.com>
2018-12-18 17:56:59 -0500

    need to close

---
+++ main.js
@@ -0,0 +1,35 @@
+// Global Variables:
+
+//word list things 
+var wordlist = ['fuck', 'shit', 'bitch', 'dick', 'pussy', 'shithead', 'asshole', 'tool'];
+
+var letterInChoosenWord = [];
+
+//computer guess
+var computerWord = ''
+
+//factor in blanks 
+
+var numBlanks = 0;
+
+//mix of blanks ans solved letters
+var blanksAndSuccesses = [];
+
+//wrong guess 
+var wrongGuess = [];
+
+//letters quessed
+
+var letters = "";
+
+//counters 
+var win = 0;
+var lose = 0;
+var guessleft = 10;
+
+//Functions: 
+//Start game --->
+function startgame(){
+//
+}
+
